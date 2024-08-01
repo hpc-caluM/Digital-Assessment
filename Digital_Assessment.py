ef3136eae7b37776f570a386817f1c25fdb556a6
hpc-caluM <20259@haurakiplains.school.nz>
2024-08-02 09:43:53 +1200

    Just got the map in yay

--- Digital_Assessment.py
+++ Digital_Assessment.py
@@ -1,1 +1,24 @@
+import pygame, sys
 
+from pygame.locals import QUIT
+pygame.init()
+clock = pygame.time.Clock()
+
+WIDTH, HEIGHT = 1920, 1080
+SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('The Creeping Silence')
+
+
+bg_surface = pygame.image.load('assets/Assessment Map.png').convert()
+bg_surface = pygame.transform.scale(bg_surface, (WIDTH, HEIGHT))
+
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+            
+
+    SCREEN.blit(bg_surface, (0,0))
+    
+    pygame.display.update()
