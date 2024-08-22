37a479d90be08d44e200fd58af40b58ac6a8a51a
hpc-caluM <20259@haurakiplains.school.nz>
2024-08-22 15:02:01 +1200

    Today yet again i broke my code and spent all class fixing it

--- Digital_Assessment.py
+++ Digital_Assessment.py
@@ -12,7 +12,7 @@ pygame.display.set_caption('The Creeping Silence')
 #Game variables (images/sprites)
 character_movement = 0
 bg_surface = pygame.image.load('assets/Assessment Map.png').convert()
-bg_surface = pygame.transform.scale(bg_surface, (WIDTH, HEIGHT))
+bg_surface = pygame.transform.scale(bg_surface,(WIDTH, HEIGHT))
 
 character_surface = pygame.image.load('assets/Character1M_3_walk_1.png').convert_alpha()       
 character_rect = character_surface.get_rect(center = (500,500))
@@ -24,10 +24,15 @@ character_surface = [pygame.image.load("assets/Character1M_3_walk_1.png"),
                 pygame.image.load("assets/Character1M_3_walk_5.png"),
                 pygame.image.load("assets/Character1M_3_walk_6.png"),
                 pygame.image.load("assets/Character1M_3_walk_7.png"),]
+clock = pygame.time.Clock()
 ANIMATION = pygame.USEREVENT
 pygame.time.set_timer(ANIMATION, 500, 500)
+run = True
+moving = False
 image_index = 0
 velocity = 12
+x = 500
+y = 500
 
 while True:
     for event in pygame.event.get():
@@ -38,6 +43,22 @@ while True:
    
     
 #The player code
+
+        if event.type == pygame.KEYUP:   
+            if event.key == pygame.K_a or event.key == pygame.K_d:
+                moving = False  
+                image_index = image_index
+        
+                if event.type == ANIMATION:
+                    if moving:
+                        if image_index < len(image_sprite)-1: 
+                            image_index += 1
+                        else:
+                            image_index = 0
+                    
+    #window.fill((0, 0, 0))
+    keys = pygame.key.get_pressed()
+
     keys = pygame.key.get_pressed() 
     if keys[pygame.K_a]: 
             character_rect.x -= 1
@@ -52,11 +73,15 @@ while True:
             
             character_movement = 0
             character_movement -= 0
+            
+    current_sprite = character_surface[image_index]
     
     SCREEN.blit(bg_surface, (0,0))
     
-    SCREEN.blit(character_surface, character_rect)
+    SCREEN.blit(character_surface[0],character_rect)
     #pygame.draw.rect(SCREEN,pygame.Color("white"),character_rect)
+    
+    #window.blit(current_sprite, (x, y))
 
     
     pygame.display.update()
