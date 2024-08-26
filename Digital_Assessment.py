86caf06d77fcb55aaa85594274f841721b9410c2
hpc-caluM <20259@haurakiplains.school.nz>
2024-08-27 11:29:18 +1200

    Achieved animation both walking and idle!

--- Digital_Assessment.py
+++ Digital_Assessment.py
@@ -1,4 +1,4 @@
-from urllib.parse import scheme_chars
+
 import pygame, sys
 
 from pygame.locals import QUIT
@@ -14,6 +14,7 @@ character_movement = 0
 bg_surface = pygame.image.load('assets/Assessment Map.png').convert()
 bg_surface = pygame.transform.scale(bg_surface,(WIDTH, HEIGHT))
 
+#Walking animation
 character_surface = pygame.image.load('assets/Character1M_3_walk_1.png').convert_alpha()       
 character_rect = character_surface.get_rect(center = (500,500))
 
@@ -23,10 +24,28 @@ character_surface = [pygame.image.load("assets/Character1M_3_walk_1.png"),
                 pygame.image.load("assets/Character1M_3_walk_4.png"),
                 pygame.image.load("assets/Character1M_3_walk_5.png"),
                 pygame.image.load("assets/Character1M_3_walk_6.png"),
-                pygame.image.load("assets/Character1M_3_walk_7.png"),]
+                pygame.image.load("assets/Character1M_3_walk_7.png")]
 clock = pygame.time.Clock()
 ANIMATION = pygame.USEREVENT
-pygame.time.set_timer(ANIMATION, 500, 500)
+pygame.time.set_timer(ANIMATION, 150)
+run = True
+moving = False
+image_index = 0
+velocity = 12
+x = 500
+y = 500
+
+#idle animation
+character_idle = [pygame.image.load("assets/Character1M_3_idle_0.png"),
+                pygame.image.load("assets/Character1M_3_idle_1.png"),
+                pygame.image.load("assets/Character1M_3_idle_2.png"),
+                pygame.image.load("assets/Character1M_3_idle_3.png"),
+                pygame.image.load("assets/Character1M_3_idle_4.png"),
+                pygame.image.load("assets/Character1M_3_idle_5.png"),
+                pygame.image.load("assets/Character1M_3_idle_6.png"),
+                pygame.image.load("assets/Character1M_3_idle_7.png")]
+ANIMATION = pygame.USEREVENT
+pygame.time.set_timer(ANIMATION, 150)
 run = True
 moving = False
 image_index = 0
@@ -47,38 +66,49 @@ while True:
         if event.type == pygame.KEYUP:   
             if event.key == pygame.K_a or event.key == pygame.K_d:
                 moving = False  
-                image_index = image_index
+                image_index = 0
         
-                if event.type == ANIMATION:
-                    if moving:
-                        if image_index < len(image_sprite)-1: 
-                            image_index += 1
-                        else:
-                            image_index = 0
+        if event.type == pygame.KEYUP:   
+            if event.key == pygame.K_w or event.key == pygame.K_s:
+                moving = False  
+                image_index = 0
+                
+        if event.type == ANIMATION:
+          
+            if image_index < len(character_surface)-1: 
+                image_index += 1
+            else:
+                image_index = 0
                     
-    #window.fill((0, 0, 0))
+   #Walking animation
     keys = pygame.key.get_pressed()
 
-    keys = pygame.key.get_pressed() 
     if keys[pygame.K_a]: 
             character_rect.x -= 1
+            moving = True
     if keys[pygame.K_d]: 
             character_rect.x += 1
+            moving = True
             
-    keys = pygame.key.get_pressed()
     if keys[pygame.K_w]:
             character_rect.y -= 1
+            moving = True
     if keys[pygame.K_s]:
             character_rect.y += 1
+            moving = True
             
             character_movement = 0
             character_movement -= 0
             
-    current_sprite = character_surface[image_index]
+    if moving:
+        current_sprite = character_surface[image_index]
+    else:
+        current_sprite = character_idle[image_index] 
     
     SCREEN.blit(bg_surface, (0,0))
     
-    SCREEN.blit(character_surface[0],character_rect)
+    SCREEN.blit(current_sprite, character_rect)
+    print(character_rect.x, character_rect.y)
     #pygame.draw.rect(SCREEN,pygame.Color("white"),character_rect)
     
     #window.blit(current_sprite, (x, y))
